81053915782320ecd066bb1df8988c8477ee172c
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-02-18 16:38:22 -0300

    linters errors fixing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@
 import './style.css';
 import addNewTodo from './addTodo.js';
 import displayAllTodos from './listTodo.js';
+// eslint-disable-next-line no-unused-vars
+// (it´s a function that will be constructed for Project 2-3)
+// eslint-disable-next-line no-unused-vars
 import Todos from './Todos.js';
 
 window.onload = () => {
